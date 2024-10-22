c643ac85e5b3f6aad2e4b36a3aeeca1434d673db
Mattias Petersson <Mattias.Petersson93@gmail.com>
2024-10-22 16:58:27 +0200

    Passing in arguments to get the CLI-version
    If a user now passes in the argument "--cli-mode" when the program is
    being ran, then the previous default behavior will execute and the user
    will be able to manage groups/sessions through their CLI.
    
    The reason for this is to start building a GUI that will be the central
    version of the program.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "practicegroup/practicegroup.h"
 #include "nlohmann/json.hpp"
 #include "utils/namespaces.cpp"
+#include "cliversion.cpp"
 #include <format>
 
 /**
@@ -22,150 +23,13 @@ void make_data()
   Group.save_to_file();
 }
 
-std::map<std::string, PracticeGroup> get_groups()
+int main(int argc, char *argv[])
 {
-  std::map<std::string, PracticeGroup> practice_groups;
-  for (auto const &t : std::filesystem::directory_iterator("data/"))
+  if (argc > 1)
   {
-    std::filesystem::path path = t.path();
-    std::string name = path.stem().string();
-    std::ifstream f(path);
-    json data = json::parse(f);
-    practice_groups.emplace(name, PracticeGroup{data});
+    std::string arg{argv[1]};
+    if (arg == "--cli-mode")
+      return cli_version();
   }
-  return practice_groups;
-}
-
-void set_active_group(std::map<std::string, PracticeGroup> &practice_groups, PracticeGroup *&active_group)
-{
-  size_t len{practice_groups.size()};
-  if (len == 0)
-  {
-    std::cout << "No groups exist yet, exiting\n";
-    return;
-  }
-  else if (len == 1)
-  {
-    auto only_element = practice_groups.begin();
-    std::cout << std::format("Only one group exists, setting group {} as active.\n", only_element->first);
-    active_group = &(only_element->second);
-    return;
-  }
-
-  std::cout << "Enter the name of the group you want to view.\n";
-  std::string groupName;
-  std::cin >> groupName;
-  auto active = practice_groups.find(groupName);
-  if (active != practice_groups.end())
-  {
-    active_group = &(active->second);
-  }
-  else
-  {
-    std::cout << "A group with the name " << groupName << " does not exist.\n";
-  }
-}
-
-void session_details(PracticeGroup *&active_group)
-{
-  std::cout << std::format("Active group is {}.\n", active_group->get_name());
-  while (true)
-  {
-    std::cout << "1. View sessions in the group\n";
-    std::cout << "2. Add session\n";
-    std::cout << "3. Remove session\n";
-    std::cout << "4. Exit\n";
-    int selection;
-    std::cin >> selection;
-    switch (selection)
-    {
-    case 1:
-      active_group->print();
-      break;
-    case 2:
-    {
-      PracticeSession session = PracticeSession::create_from_input(std::cin);
-      session.print();
-      active_group->add_session(session);
-      break;
-    }
-    case 3:
-    {
-      std::cin.ignore();
-      std::cout << "Date of session to remove\n";
-      C::year_month_day date;
-      C::from_stream(std::cin, "%F", date);
-      active_group->remove_session(date);
-      break;
-    }
-    default:
-      return;
-    }
-  }
-}
-
-int main(void)
-{
-  make_data();
-  std::map<std::string, PracticeGroup> practice_groups = get_groups();
-  bool hasChanged = false;
-  while (true)
-  {
-    std::cout << "1. Show all groups\n";
-    std::cout << "2. View/Add/Remove Sessions\n";
-    std::cout << "3. Create group\n";
-    std::cout << "4. Exit\n";
-    int selection;
-    std::cin >> selection;
-
-    switch (selection)
-    {
-    case 1:
-    {
-      for (auto const &group : practice_groups)
-      {
-        std::cout << group.first << "\n";
-      }
-      break;
-    }
-    case 2:
-    {
-      PracticeGroup *active_group = nullptr;
-      set_active_group(practice_groups, active_group);
-      if (active_group != nullptr)
-      {
-        session_details(active_group);
-      }
-      break;
-    }
-    case 3:
-    {
-      std::cout << "Enter the name of the new group.\n";
-      while (true)
-      {
-        std::string groupName;
-        std::cin >> groupName;
-        if (practice_groups.contains(groupName))
-        {
-          std::cout << "Group with that name already exists, try again.\n";
-        }
-        else
-        {
-          PracticeGroup{groupName};
-          std::cout << std::format("Group named {} created\n", groupName);
-          hasChanged = true;
-          break;
-        }
-      }
-      break;
-    }
-    default:
-      return 0; // A number for exiting is given, but any input that's not valid will do.
-    }
-    if (hasChanged)
-    {
-      practice_groups = get_groups();
-      hasChanged = false;
-    }
-  }
+  std::cout << "No argument.";
 }
